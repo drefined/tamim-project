5c38f3897330141bcf5dbb2a3db0526e50e623c0
David Phruksukarn <d.refined@gmail.com>
2015-12-31 00:23:09 -0800

    auto format, add rest endpoints to add, update, delete ragas

--- devServer.js
+++ devServer.js
@@ -11,9 +11,9 @@ var nodeEnv = process.env.NODE_ENV;
 
 var port = 3000;
 
-if (!_.isEmpty(nodeEnv) && nodeEnv == 'test') {
+if (!_.isEmpty(nodeEnv) && nodeEnv === 'test') {
   port = 4000;
-};
+}
 
 const db = 'tamim';
 const table = 'ragas';
@@ -25,7 +25,7 @@ app.use(bodyParser.json()); // application/json
 app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded
 
 // handle Bad JSON
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     console.log('Bad JSON');
     next();
@@ -34,30 +34,92 @@ app.use(function(err, req, res, next) {
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(express.static(path.join(__dirname, '/src')));
 
-app.get('/', function(req, res) {
+app.get('/', function index(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/ragas', function(req, res) {
+app.get('/ragas', function listRagas(req, res) {
   co(function *() {
     yield r.db(db)
       .table(table)
       .run()
-      .then(function(result) {
+      .then(function (result) {
         res.json(result);
       });
   });
 });
 
+app.post('/ragas', function addRaga(req, res) {
+  co(function *() {
+    var body = req.body;
 
-app.listen(port, 'localhost', function(err) {
+    if (!_.isEmpty(body) && _.isObject(body)) {
+      yield r.db(db)
+        .table(table)
+        .insert(body)
+        .run()
+        .then(function (result) {
+          var ragaId = _.first(result.generated_keys);
+          res.json({ id: ragaId });
+        })
+        .catch(function (e) {
+          res.status(500).json({ error: 'something bad happened.' });
+        });
+    } else {
+      res.status(406).json({ error: 'request contained bad json or empty body.' });
+    }
+  });
+});
+
+app.put('/ragas/:id', function updateRaga(req, res) {
+  co(function *() {
+    var id = req.params.id;
+    var body = req.body;
+
+    if (!_.isEmpty(body) && _.isObject(body)) {
+      yield r.db(db)
+        .table(table)
+        .get(id)
+        .update(body)
+        .run()
+        .then(function (result) {
+          res.json(result);
+        })
+        .catch(function (e) {
+          res.status(500).json({ error: 'something bad happened.' });
+        });
+    } else {
+      res.status(406).json({ error: 'request contained bad json or empty body.' });
+    }
+  });
+});
+
+app.delete('/ragas/:id', function deleteRaga(req, res) {
+  co(function *() {
+    var id = req.params.id;
+
+    yield r.db(db)
+      .table(table)
+      .get(id)
+      .delete()
+      .run()
+      .then(function (result) {
+        res.json(result);
+      })
+      .catch(function (e) {
+        res.status(500).json({ error: 'something bad happened.' });
+      });
+  });
+});
+
+app.listen(port, 'localhost', function startServer(err) {
   if (err) {
     console.log(err);
     return;
