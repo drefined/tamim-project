33a8fac4eeb1870ccdd6a6247671b4197ffcdd2f
David Phruksukarn <d.refined@gmail.com>
2015-12-30 18:14:46 -0800

    add airbnb eslint configuration, test linter on test data which warns about missing trailing comma

--- db/data.js
+++ db/data.js
@@ -1,89 +1,90 @@
 module.exports = [
   {
-    name : 'AnandaBhairavi',
-    medicalConditions : ['hypertension', 'lower blood pressure'],
-    summary : `Has been used clinically to lower high blood pressure and treat hypertension. As a preventative measure, it is recommened to listen to this raga before taking on strenuous, or highly concentrated tasks or jobs, because of its relaxing effects.`,
-    asc : `S G1 R2 G1 M1 P D2 P S'`,
-    desc : `S' N1 D2 P M1 G1 R2 S`,
-    ascWestern : `Eb Gb F Gb Ab Bb C Bb Eb'`,
-    descWestern : `Eb' Db C B Ab Gb F Eb`,
-    histogram : ['image1','image2']
+    name: 'AnandaBhairavi',
+    medicalConditions: ['hypertension', 'lower blood pressure'],
+    summary: `Has been used clinically to lower high blood pressure and treat hypertension. As a preventative measure, it is recommened to listen to this raga before taking on strenuous, or highly concentrated tasks or jobs, because of its relaxing effects.`,
+    asc: `S G1 R2 G1 M1 P D2 P S'`,
+    desc: `S' N1 D2 P M1 G1 R2 S`,
+    ascWestern: `Eb Gb F Gb Ab Bb C Bb Eb'`,
+    descWestern: `Eb' Db C B Ab Gb F Eb`,
+    histogram: ['image1', 'image2'],
+    audio: ['file1', 'file2'],
   },
   {
-    name : 'Bagesri',
-    medicalConditions : ['insomnia'],
-    summary : `Recommended to help those effected by insomnia or those in need of a sleep aid. Should be used at night before bed.`
+    name: 'Bagesri',
+    medicalConditions: ['insomnia'],
+    summary: `Recommended to help those effected by insomnia or those in need of a sleep aid. Should be used at night before bed.`,
   },
   {
-    name : 'Bhimplas',
-    medicalConditions : ['anxiety']
+    name: 'Bhimplas',
+    medicalConditions: ['anxiety'],
   },
   {
-    name : 'Bilahari',
-    medicalConditions : ['relaxation', 'rejuvenation', 'anti-depressant']
+    name: 'Bilahari',
+    medicalConditions: ['relaxation', 'rejuvenation', 'anti-depressant'],
   },
   {
-    name : 'Charukesi',
-    medicalConditions : ['heart']
+    name: 'Charukesi',
+    medicalConditions: ['heart'],
   },
   {
-    name : 'Hamsadhwani',
-    medicalConditions : ['energizer']
+    name: 'Hamsadhwani',
+    medicalConditions: ['energizer'],
   },
   {
-    name : 'Hindolam',
-    medicalConditions : ['backpain']
+    name: 'Hindolam',
+    medicalConditions: ['backpain'],
   },
   {
-    name : 'Kaanada',
-    medicalConditions : ['weight', 'belly fat']
+    name: 'Kaanada',
+    medicalConditions: ['weight', 'belly fat'],
   },
   {
-    name : 'Kalyani',
-    medicalConditions : ['heart ailments']
+    name: 'Kalyani',
+    medicalConditions: ['heart ailments'],
   },
   {
-    name : 'Kambhoji',
-    medicalConditions : ['stomach', 'digestion']
+    name: 'Kambhoji',
+    medicalConditions: ['stomach', 'digestion'],
   },
   {
-    name : 'Karaharapriya',
-    medicalConditions : []
+    name: 'Karaharapriya',
+    medicalConditions: [],
   },
   {
-    name : 'Kedaram',
-    medicalConditions : ['cold', 'asthma', 'pulmonary', 'cough']
+    name: 'Kedaram',
+    medicalConditions: ['cold', 'asthma', 'pulmonary', 'cough'],
   },
   {
-    name : 'Poorvikalyani',
-    medicalConditions : ['eyes']
+    name: 'Poorvikalyani',
+    medicalConditions: ['eyes'],
   },
   {
-    name : 'Saama',
-    medicalConditions : ['peace of mind']
+    name: 'Saama',
+    medicalConditions: ['peace of mind'],
   },
   {
-    name : 'Sahana',
-    medicalConditions : ['anger management']
+    name: 'Sahana',
+    medicalConditions: ['anger management'],
   },
   {
-    name : 'Sankarabaranam',
-    medicalConditions : ['depression','heart']
+    name: 'Sankarabaranam',
+    medicalConditions: ['depression', 'heart'],
   },
   {
-    name : 'Saveri',
-    medicalConditions : ['stomach', 'digestion']
+    name: 'Saveri',
+    medicalConditions: ['stomach', 'digestion'],
   },
   {
-    name : 'SinduBhairavi',
-    medicalConditions : ['sinusitis', 'arthritis', 'asthma']
+    name: 'SinduBhairavi',
+    medicalConditions: ['sinusitis', 'arthritis', 'asthma'],
   },
   {
-    name : 'Thodi',
-    medicalConditions : ['headache', 'heart', 'anxiety']
+    name: 'Thodi',
+    medicalConditions: ['headache', 'heart', 'anxiety'],
   },
   {
-    name : 'Vasantha',
-    medicalConditions : ['blood purifier', 'gastritis', 'hypertension', 'blood']
+    name: 'Vasantha',
+    medicalConditions: ['blood purifier', 'gastritis', 'hypertension', 'blood'],
   },
-]
+];
