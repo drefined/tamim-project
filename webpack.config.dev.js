7f9ae87c43853fa12fc73bd08168a1e953416547
David Phruksukarn <d.refined@gmail.com>
2015-12-31 00:21:07 -0800

    auto format

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,33 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         loaders: ['babel'],
-        include: path.join(__dirname, 'src')
+        include: path.join(__dirname, 'src'),
       },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader!autoprefixer-loader?browsers=last 10 version'},
       { test: /\.tff|.woff2|.woff|.svg|.eot|.tff/, loader: 'null' },
-      { test: /\.jpg|.png/, loader: 'url' }
-    ]
-  }
+      { test: /\.jpg|.png/, loader: 'url' },
+    ],
+  },
 };
